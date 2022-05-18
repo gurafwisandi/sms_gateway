ea9375ca0a215c375c08d92915a669d38f743efc
Gepezet <gepezet91@gmail.com>
2022-05-18 16:00:17 +0700

    update SS, footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,11 +2,10 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                2020 © Xoric.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-right d-none d-sm-block">
-                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
+                    2022 © MA Ma'arif 9 Kotagajah
                 </div>
             </div>
         </div>
